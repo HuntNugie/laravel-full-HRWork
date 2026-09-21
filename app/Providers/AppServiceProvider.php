9a2c9fe713b6eb232968f186066d4146c2a0251a
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 17:14:46 +0700

    feat: register work management policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\DivisionProject;
+use App\Models\MasterProject;
+use App\Models\Task;
+use App\Policies\DivisionProjectPolicy;
+use App\Policies\MasterProjectPolicy;
 use App\Policies\RolePolicy;
+use App\Policies\TaskPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Models\Role;
@@ -23,5 +29,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Role::class, RolePolicy::class);
+        Gate::policy(MasterProject::class, MasterProjectPolicy::class);
+        Gate::policy(DivisionProject::class, DivisionProjectPolicy::class);
+        Gate::policy(Task::class, TaskPolicy::class);
     }
 }
