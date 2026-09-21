46dbd00c9b8c9eb024b6c342f6ad2a58ed9ad93d
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 16:42:01 +0700

    feat(work-management): seed work management roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,26 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-
-        $hr =  Role::create([
-            'name' => 'HR',
-            'guard_name' => 'web'
-        ]);
-        $admin =  Role::create([
-            'name' => 'Administrator',
-            'guard_name' => 'web'
-        ]);
+        foreach (['HR', 'Administrator', 'task-worker', 'supervisor', 'manager', 'general-manager'] as $roleName) {
+            Role::firstOrCreate([
+                'name' => $roleName,
+                'guard_name' => 'web',
+            ]);
+        }
     }
 }
