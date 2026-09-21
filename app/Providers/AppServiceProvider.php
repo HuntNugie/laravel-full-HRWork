03e85c4fa54051e04ffc48b4f71539ace8853592
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 17:24:59 +0700

    refactor: move work management routes out of service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Livewire\Page\Main\WorkManagement\CreateMasterProject;
-use App\Livewire\Page\Main\WorkManagement\MasterProjects;
 use App\Models\DivisionProject;
 use App\Models\MasterProject;
 use App\Models\Task;
@@ -12,7 +10,6 @@ use App\Policies\MasterProjectPolicy;
 use App\Policies\RolePolicy;
 use App\Policies\TaskPolicy;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Models\Role;
 
@@ -29,15 +26,5 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(MasterProject::class, MasterProjectPolicy::class);
         Gate::policy(DivisionProject::class, DivisionProjectPolicy::class);
         Gate::policy(Task::class, TaskPolicy::class);
-
-        Route::middleware(['web', 'auth', 'isActive'])->group(function (): void {
-            Route::get('/work-management/master-projects', MasterProjects::class)
-                ->middleware('permission:view-master-project')
-                ->name('work-management.master-projects');
-
-            Route::get('/work-management/master-projects/create', CreateMasterProject::class)
-                ->middleware('permission:create-master-project')
-                ->name('work-management.master-projects.create');
-        });
     }
 }
