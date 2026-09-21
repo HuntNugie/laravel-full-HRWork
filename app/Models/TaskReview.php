36b9d14bd592858931ca97c62767e1d345d2d7e4
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 17:11:50 +0700

    feat(work-management): add TaskReview model

---
+++ app/Models/TaskReview.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Guarded;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Guarded('id')]
+class TaskReview extends Model
+{
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function reviewer(): BelongsTo
+    {
+        return $this->belongsTo(Employees::class, 'reviewer_id');
+    }
+}
