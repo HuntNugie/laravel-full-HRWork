42a16fb6e5cb2cc79f0adbf9cf00c5d6354029b1
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 17:12:24 +0700

    feat(work-management): extend Team relations

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 #[Guarded('id')]
 class Team extends Model
@@ -29,5 +30,17 @@ class Team extends Model
     public function supervisor()
     {
         return $this->belongsTo(Employees::class, 'supervisor_id');
+    }
+
+    public function divisionProjects(): BelongsToMany
+    {
+        return $this->belongsToMany(DivisionProject::class, 'project_teams')
+            ->withPivot('assigned_by')
+            ->withTimestamps();
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 }
