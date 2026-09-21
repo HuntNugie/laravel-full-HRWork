b9a5174f6475ede1b18b79725476ee5eaafaa61f
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-21 17:12:39 +0700

    feat(work-management): add Work Management permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
 {
@@ -14,9 +12,36 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $permissions = ['view-divisi', 'create-divisi', 'update-divisi', 'delete-divisi', 'show-divisi', 'view-position', 'create-position', 'view-employee', 'create-employee', 'view-team', 'view-status-daily'];
+        $permissions = [
+            'view-divisi',
+            'create-divisi',
+            'update-divisi',
+            'delete-divisi',
+            'show-divisi',
+            'view-position',
+            'create-position',
+            'view-employee',
+            'create-employee',
+            'view-team',
+            'view-status-daily',
+            'view-master-project',
+            'create-master-project',
+            'update-master-project',
+            'view-division-project',
+            'create-division-project',
+            'update-division-project',
+            'view-task',
+            'create-task',
+            'update-task',
+            'submit-task',
+            'review-task',
+            'report-project-progress',
+            'submit-project-review',
+            'review-project',
+        ];
+
         foreach ($permissions as $permission) {
-            Permission::create([
+            Permission::firstOrCreate([
                 'name' => $permission,
             ]);
         }
