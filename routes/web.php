4a20a11799a77a9717db17414baf44aae2e259fa
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-22 16:18:03 +0700

    feat: create tasks inside project team pages

--- routes/web.php
+++ routes/web.php
@@ -228,7 +228,7 @@ Route::middleware(['auth', 'isActive'])->group(function () {
         Route::get('/master-projects/{masterProject}/division-projects/create', CreateDivisionProject::class)->middleware('permission:create-division-project')->name('work-management.master-projects.division-projects.create');
         Route::get('/master-projects/{masterProject}/approve', MasterProjectApprove::class)->middleware('permission:approve-master-project')->name('work-management.master-projects.approve');
         Route::get('/division-projects/{divisionProject}', DivisionProjectDetail::class)->middleware('permission:view-division-project')->name('work-management.division-projects.show');
-        Route::get('/division-projects/{divisionProject}/tasks/create', CreateTask::class)->middleware('permission:create-task')->name('work-management.division-projects.tasks.create');
+        Route::get('/division-projects/{divisionProject}/teams/{team}/tasks/create', CreateTask::class)->middleware('permission:view-task')->name('work-management.division-projects.tasks.create');
         Route::get('/tasks', MyTasks::class)->middleware('permission:view-task')->name('work-management.tasks');
         Route::get('/tasks/{task}', TaskDetail::class)->middleware('permission:view-task')->name('work-management.tasks.show');
     });
