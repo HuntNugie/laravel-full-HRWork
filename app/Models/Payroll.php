9032e62e9340da0160e655f18dc33505de37e00e
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-23 20:36:00 +0700

    refactor: remove resignation payroll relation

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -56,12 +56,4 @@ class Payroll extends Model
             'employee_contract_id'
         );
     }
-
-    public function resignation(): BelongsTo
-    {
-        return $this->belongsTo(
-            EmployeeResignation::class,
-            'resignation_id'
-        );
-    }
 }
