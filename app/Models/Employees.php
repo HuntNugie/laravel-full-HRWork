99aa70e96df1dfce827bd453a6e8dfccc38073fe
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-23 19:55:30 +0700

    feat(resignation): add employee resignation relationship

--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -98,4 +98,9 @@ class Employees extends Model
     {
         return $this->hasMany(LeaveRequest::class, 'employee_id');
     }
+
+    public function resignations()
+    {
+        return $this->hasMany(EmployeeResignation::class, 'employee_id');
+    }
 }
