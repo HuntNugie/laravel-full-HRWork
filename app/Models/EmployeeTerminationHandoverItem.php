f78cc91cfabb3d9b8797dfb0482b992c61676709
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-23 23:24:36 +0700

    feat: store employee exit action state

--- app/Models/EmployeeTerminationHandoverItem.php
+++ app/Models/EmployeeTerminationHandoverItem.php
@@ -32,4 +32,12 @@ class EmployeeTerminationHandoverItem extends Model
     {
         return $this->belongsTo(User::class, 'verified_by');
     }
+
+    protected function casts(): array
+    {
+        return [
+            'action_state' => 'array',
+            'verified_at' => 'datetime',
+        ];
+    }
 }
