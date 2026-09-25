4e463cb126c45852a6058cea7c72267541e7dfa7
Nugie kurniawan <nugiebinhar@gmail.com>
2026-09-25 20:02:07 +0700

    refactor(work-management): protect task creation by create permission

--- routes/web.php
+++ routes/web.php
@@ -283,7 +283,7 @@ Route::middleware(['auth', 'isActive'])->group(function () {
         Route::get('/master-projects/{masterProject}/approve', MasterProjectApprove::class)->middleware('permission:approve-master-project')->name('work-management.master-projects.approve');
         Route::get('/division-projects/{divisionProject}', DivisionProjectDetail::class)->middleware('permission:view-division-project')->name('work-management.division-projects.show');
         Route::get('/division-projects/{divisionProject}/teams/{team}', DivisionProjectTeamDetail::class)->middleware('permission:view-division-project')->name('work-management.division-projects.teams.show');
-        Route::get('/division-projects/{divisionProject}/teams/{team}/tasks/create', CreateTask::class)->middleware('permission:view-task')->name('work-management.division-projects.tasks.create');
+        Route::get('/division-projects/{divisionProject}/teams/{team}/tasks/create', CreateTask::class)->middleware('permission:create-task')->name('work-management.division-projects.tasks.create');
         Route::get('/tasks', MyTasks::class)->middleware('permission:view-task')->name('work-management.tasks');
         Route::get('/tasks/{task}', TaskDetail::class)->middleware('permission:view-task')->name('work-management.tasks.show');
     });
